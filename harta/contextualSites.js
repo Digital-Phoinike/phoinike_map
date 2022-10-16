120442969bec541cb331c8d022bdeef3f160bbeb
mnaglak <51410453+mnaglak@users.noreply.github.com>
2022-10-16 13:15:42 -0400

    contextual map draft

--- harta/contextualSites.js
+++ harta/contextualSites.js
@@ -1,185 +1,150 @@
-
-  var sites = {  "features": [
-        {
-            "geometry": {
-                "coordinates": [
-                    20.1050058,
-                    39.6987314
-                ],
-                "type": "Point"
-            },
-            "properties": {
-                "description": "Malathrea",
-                "name": "Malathrea",
-                "type": "Site"
-            },
-            "type": "Feature"
-        },
-        {
-            "geometry": {
-                "coordinates": [
-                    20.1224891,
-                    39.6816308
-                ],
-                "type": "Point"
-            },
-            "properties": {
-                "description": "Çuka e Ajtoit",
-                "name": "Çuka e Ajtoit",
-                "type": "Site"
-            },
-            "type": "Feature"
-        },
-        {
-            "geometry": {
-                "coordinates": [
-                    20.0345619,
-                    39.8474024
-                ],
-                "type": "Point"
-            },
-            "properties": {
-                "description": "Çuka e Sarandës - Vila\n",
-                "name": "Çuka e Sarandës - Vila\n",
-                "type": "Site"
-            },
-            "type": "Feature"
-        },
-        {
-            "geometry": {
-                "coordinates": [
-                    20.1004879,
-                    39.9057953
-                ],
-                "type": "Point"
-            },
-            "properties": {
-                "description": "Mesopotam - Monastery\n",
-                "name": "Mesopotam - Monastery\n",
-                "type": "Site"
-            },
-            "type": "Feature"
-        },
-        {
-            "geometry": {
-                "coordinates": [
-                    20.033414,
-                    39.872353
-                ],
-                "type": "Point"
-            },
-            "properties": {
-                "description": "Metoq villa\n",
-                "name": "Metoq villa\n",
-                "type": "Site"
-            },
-            "type": "Feature"
-        },
-        {
-            "geometry": {
-                "coordinates": [
-                    20.0052703,
-                    39.8729699
-                ],
-                "type": "Point"
-            },
-            "properties": {
-                "description": "Roman wall fragment - Saranda (Onchesmos)\n",
-                "name": "Roman wall fragment - Saranda (Onchesmos)\n",
-                "type": "Site"
-            },
-            "type": "Feature"
-        },
-        {
-            "geometry": {
-                "coordinates": [
-                    20.0079858,
-                    39.87448
-                ],
-                "type": "Point"
-            },
-            "properties": {
-                "description": "Mosaic in basement",
-                "name": "Museum Of Archaeology",
-                "type": "Site"
-            },
-            "type": "Feature"
-        },
-        {
-            "geometry": {
-                "coordinates": [
-                    20.0066016,
-                    39.8747353
-                ],
-                "type": "Point"
-            },
-            "properties": {
-                "description": "Synagogue - Basilica, Archeological Remains",
-                "name": "Synagogue - Basilica, Archeological Remains",
-                "type": "Site"
-            },
-            "type": "Feature"
-        },
-        {
-            "geometry": {
-                "coordinates": [
-                    20.0565156,
-                    39.9135125
-                ],
-                "type": "Point"
-            },
-            "properties": {
-                "description": "Finiq Archaeological Park",
-                "name": "Finiq Archaeological Park",
-                "type": "Finiq"
-            },
-            "type": "Feature"
-        },
-        {
-            "geometry": {
-                "coordinates": [
-                    20.0509874,
-                    39.7601511
-                ],
-                "type": "Point"
-            },
-            "properties": {
-                "description": "Diaporit - Roman Villa\n",
-                "name": "Diaporit - Roman Villa\n",
-                "type": "Possible Site"
-            },
-            "type": "Feature"
-        },
-        {
-            "geometry": {
-                "coordinates": [
-                    20.0599074,
-                    39.9328331
-                ],
-                "type": "Point"
-            },
-            "properties": {
-                "description": "Matomara",
-                "name": "Matomara",
-                "type": "Possible Site"
-            },
-            "type": "Feature"
-        },
-        {
-            "geometry": {
-                "coordinates": [
-                    19.856094,
-                    40.069753
-                ],
-                "type": "Point"
-            },
-            "properties": {
-                "description": "The Castle of Borsh",
-                "name": "The Castle of Borsh",
-                "type": "Possible Site"
-            },
-            "type": "Feature"
-        }
-    ],
-    "type": "FeatureCollection"
+var contextualSites = {
+	"type" : "FeatureCollection",
+	"name" : "Placemark_point",
+	"features" : [
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 20.1050058, 39.6987314, 0 ]
+			},
+			"properties" : {
+				"OBJECTID" : 1,
+				"Location" : "Malathrea",
+				"Type" : "Site"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 20.1224891, 39.6816308, 0 ]
+			},
+			"properties" : {
+				"OBJECTID" : 2,
+				"Location" : "Çuka e Ajtoit",
+				"Type" : "Site"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 20.0345619, 39.8474024, 0 ]
+			},
+			"properties" : {
+				"OBJECTID" : 3,
+				"Location" : "Çuka e Sarandës - Vila",
+				"Type" : "Site"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 20.1004879, 39.9057953, 0 ]
+			},
+			"properties" : {
+				"OBJECTID" : 4,
+				"Location" : "Mesopotam - Monastery",
+				"Type" : "Site"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 20.033414, 39.872353, 0 ]
+			},
+			"properties" : {
+				"OBJECTID" : 5,
+				"Location" : "Metoq villa",
+				"Type" : "Site"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 20.0052703, 39.8729699, 0 ]
+			},
+			"properties" : {
+				"OBJECTID" : 6,
+				"Location" : "Roman wall fragment - Saranda (Onchesmos)",
+				"Type" : "Site"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 20.0079858, 39.87448, 0 ]
+			},
+			"properties" : {
+				"OBJECTID" : 7,
+				"Location" : "Museum Of Archaeology",
+				"Type" : "Site"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 20.0066016, 39.8747353, 0 ]
+			},
+			"properties" : {
+				"OBJECTID" : 8,
+				"Location" : "Synagogue - Basilica, Archeological Remains",
+				"Type" : "Site"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 20.0565156, 39.9135125, 0 ]
+			},
+			"properties" : {
+				"OBJECTID" : 9,
+				"Location" : "Finiq Archaeological Park",
+				"Type" : "Finiq"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 20.0509874, 39.7601511, 0 ]
+			},
+			"properties" : {
+				"OBJECTID" : 10,
+				"Location" : "Diaporit - Roman Villa",
+				"Type" : "Possible Site"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 20.0599074, 39.9328331, 0 ]
+			},
+			"properties" : {
+				"OBJECTID" : 11,
+				"Location" : "Matomara",
+				"Type" : "Possible Site"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ 19.856094, 40.069753, 0 ]
+			},
+			"properties" : {
+				"OBJECTID" : 12,
+				"Location" : "The Castle of Borsh\r\nThe Castle of Borsh\r\nThe Castle of Borsh",
+				"Type" : "Possible Site"
+			}
+		}
+	]
 }
